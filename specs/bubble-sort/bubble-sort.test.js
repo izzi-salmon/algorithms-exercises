a08369adda8fa5487fdccef1d7573c59b05408cc
Izzi Salmon <izzi.salmon@gmail.com>
2024-09-03 10:59:30 +1200

    optimisations

--- specs/bubble-sort/bubble-sort.test.js
+++ specs/bubble-sort/bubble-sort.test.js
@@ -15,13 +15,17 @@ function bubbleSort(nums) {
   // Set that the list is not sorted yet
   let isListSorted = false;
 
+  // Create a variable to store the amount of iterations we need to go through
+  // Set this initially to the length of the whole array
+  let iterations = nums.length;
+
   // While: list is not sorted
   while (!isListSorted) {
 
     let swapCounter = 0;
 
     // For each item in the array, compare if the next item in the sequence is larger
-    for (let index = 0; index < nums.length; index++) {
+    for (let index = 0; index < iterations; index++) {
       // If the next item is smaller, swap them
       if (nums[index] > nums[index + 1]){ 
         let temp = nums[index]; // Store the first item in a temporary variable
@@ -29,12 +33,15 @@ function bubbleSort(nums) {
         nums[index + 1] = temp; // Set the second item's value to the first
 
         // Increment the swap counter to note whether anything was swapped
-        swapCounter ++; 
+        swapCounter ++;
       }
 
       // If the next item is larger, go to the next item in the sequence
       
     }
+
+    // Remove one iteration each time as we sort through the loop as we know the last items have already been sorted
+    iterations --;
 
     // Once all numbers have been iterated through, check if anything was swapped
     // If nothing was swapped, set the list sorted to be true
