06f4dab584b71fcd41daad793b04ece4697c290f
Izzi Salmon <izzi.salmon@gmail.com>
2024-09-09 13:15:25 +1200

    merge sort solution

--- specs/merge-sort/merge-sort.test.js
+++ specs/merge-sort/merge-sort.test.js
@@ -6,97 +6,47 @@
   You'll need to write more than just one function
 */
 
-// Move spliting logic into seperate function?
-const splitArray = (array) => {
-  // base case: Return sorted list
-
-  // This part doesn't feel right, what is the best way to write this?
-  if (array.length === 1){
-    // TODO: write 0 case ???
-      // console.log("return sorted list: " + array[0]);
-      return array[0];
-    
-  }
-
-  const index = Math.round(array.length / 2);
-
-  const firstHalf = array.slice(0, index);
-  const secondHalf = array.slice(index);
-
-  // console.log(firstHalf);
-  // console.log(secondHalf);
-
-  let parentArray = [];
-
-  parentArray.push(firstHalf);
-  parentArray.push(secondHalf);
-
-  // console.log(parentArray);
-
-  for (let i = 0; i < parentArray.length; i++) {
-    splitArray(parentArray[i]);
-  }
-}
-
 const mergeSort = (nums) => {
 
-  // Why does it keep returning undefined sometimes here?
-  splitArray(nums);
-
-  // How do I get two items into my merge function when I'm only returning one from splitArray?
-
-  // const sortedResult = splitArray(nums);
-  // let resultsArray = [];
-  
-  // resultsArray.push(splitArray(nums));
-
-  // console.log(resultsArray);
-  // console.log(splitArray(nums));
-
-  // console.log(sortedResult);
-
-
-  // [10, 5, 3, 8, 2, 6, 4, 7, 9, 1]
-  // Break the large array into two smaller arrays
-  
-  // [10, 5, 3, 8, 2], [6, 4, 7, 9, 1]
-  // Break those arrays into two smaller arrays
-  // [10, 5, 3], [8, 2] // [6, 4, 7], [9, 1]
-  // Break those arrays into two smaller arrays
-  // [10, 5], [3] // [8], [2] // [6, 4], [7] // [9], [10]
-  // Break the larger arrays into smaller ones again
-  // [10], [5] // [3] // [8], [2] // [6], [4] // 7 // [9], [10]
-  // apply base case to already sorted (array length of one) arrays, return sorted list
-  // [10], [5]
-
-  
-
-  // which is the smallest item? Add to end of new array
-  // [10], [] => [5]
-  // One of the arrays is empty, concat the leftover array
-  // [5, 10]
-  // [5, 10], [3]
-  // which is the smallest item of 5 and 3? Add to end of new array
-  // [5, 10], [] => [3]
-  // which is the smallest item of 5 and 10? Add to end of array
-  // [10], [] => [3, 5]
-  // concat leftover
-  // [3, 5, 10]
-};
-
-const merge = (itemA, itemB) => {
-  let mergedItems = [];
-  
-  if(itemA < itemB){
-    mergedItems.push(itemA);
-    mergedItems.push(itemB);
-  } else {
-    mergedItems.push(itemB);
-    mergedItems.push(itemA);
+  if (nums.length < 2){
+      return nums;
   }
 
-  return mergedItems;
-}
+  const index = Math.floor(nums.length / 2);
+
+  const firstHalf = nums.slice(0, index);
+  const secondHalf = nums.slice(index);
+
+  console.log(firstHalf);
+  console.log(secondHalf);
+
+  const sortedFirstHalf = mergeSort(firstHalf);
+  const sortedSecondHalf = mergeSort(secondHalf);
+
+  return merge(sortedFirstHalf, sortedSecondHalf);
+
+};
+
+const merge = (array1, array2) => {
+
+  const results = [];
+
+  // Go until one array is empty
+  while(array1.length && array2.length) {
+    // Compare the first items of the arrays to find the smallest
+    if(array1[0] <= array2[0]) {
+      // If smaller, remove the first item of the first array and add it to the end of results
+      results.push(array1.shift());
+    } else {
+      // If smaller, remove the first item of the second array and add it to the end of results
+      results.push(array2.shift());
+    }
+  }
+
+  // Add the remaining largest value to the Results
+  // concat will add anything that is remaining, ignorning empty arrays
+  return results.concat(array1, array2);
+};
 
 // unit tests
 // do not modify the below code
